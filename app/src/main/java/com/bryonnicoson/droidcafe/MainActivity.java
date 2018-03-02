8f04282e653c0885d861ac57eaa0c58f7783e454
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-02 08:04:18 -0600

    OrderActivity

--- app/src/main/java/com/bryonnicoson/droidcafe/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/droidcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bryonnicoson.droidcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
      */
     public void showFoodOrder(String message) {
         displayToast(message);
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
     }
 
     public void displayToast(String message){
